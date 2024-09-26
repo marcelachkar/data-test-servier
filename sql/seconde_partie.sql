ecdd95c012f0a07ab97ce9ac56ae543b90558b3b
Marc El Achkar <marc@achkar.com>
2024-09-26 15:31:40 +0200

    data test servier

--- sql/seconde_partie.sql
+++ sql/seconde_partie.sql
@@ -1,33 +1,18 @@
 -- Seconde partie du test
-WITH ventes_meuble AS (
-  SELECT
-    t_table.client_id AS client_id,
-    SUM(t_table.prod_price * t_table.prod_qty) AS ventes_meuble
-  FROM
-    TRANSACTION t_table
-    INNER JOIN PRODUCT_NOMENCLATURE pn_table ON t_table.prod_id = pn_table.product_id
-  WHERE
-    t_table.date BETWEEN '2020-01-01' AND '2020-12-31'
-    AND pn_table.product_type = 'MEUBLE'
-  GROUP BY
-    t_table.client_id
-), ventes_deco AS (
-  SELECT
-    t_table.client_id AS client_id,
-    SUM(t_table.prod_price * t_table.prod_qty) AS ventes_deco
-  FROM
-    TRANSACTION t_table
-    INNER JOIN PRODUCT_NOMENCLATURE pn_table ON t_table.prod_id = pn_table.product_id
-  WHERE
-    t_table.date BETWEEN '2020-01-01' AND '2020-12-31'
-    AND pn_table.product_type = 'DECO'
-  GROUP BY
-    t_table.client_id
-)
 SELECT
-    COALESCE(vm_table.client_id, vd_table.client_id) AS client_id,
-    COALESCE(vm_table.ventes_meuble, 0) AS ventes_meuble,
-    COALESCE(vd_table.ventes_deco, 0) AS ventes_deco
+    t_table.client_id,
+    SUM(CASE
+            WHEN pn_table.product_type = 'MEUBLE' THEN (t_table.prod_price * t_table.prod_qty)
+            ELSE 0
+        END) AS ventes_meuble,
+    SUM(CASE
+            WHEN pn_table.product_type = 'DECO' THEN (t_table.prod_price * t_table.prod_qty)
+            ELSE 0
+        END) AS ventes_deco
 FROM
-    ventes_meuble AS vm_table
-FULL OUTER JOIN ventes_deco AS vd_table ON vm_table.client_id = vd_table.client_id;
+    TRANSACTION t_table
+    INNER JOIN PRODUCT_NOMENCLATURE pn_table ON t_table.prod_id = pn_table.product_id
+WHERE
+    t_table.date BETWEEN '2020-01-01' AND '2020-12-31'
+GROUP BY
+    t_table.client_id;
